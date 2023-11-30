afcf904011cabea103a64b0f2af421d1bdea3b61
Evanlaksana <132762464+Evanlaksana@users.noreply.github.com>
2023-11-30 11:35:20 +0700

    Add files via upload

---
+++ Latihan55-Handphone/src/latihan55/handphone/Android.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latihan55.handphone;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Android extends Handphone{
+    private String keyStore;
+    
+    public Android(String man, String os, String model, int harga) {
+        super(man, os, model, harga);
+        this.manufacture = man;
+        this.operatingSystem = os;
+        this.model = model;
+        this.harga = harga;
+
+    }
+
+    public String getKeyStore() {
+        return keyStore;
+    }
+
+    public void setKeyStore(String keyStore) {
+        this.keyStore = keyStore;
+    }   
+}
